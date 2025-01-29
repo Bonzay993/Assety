427e5c38622a1eaf1f953e0803f313d76e83557c
Ionut Blanaru <blanaru.ionut@gmail.com>
2025-01-29 15:14:56 +0000

    Added the basic design from which can get expanded

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
-from flask import Flask, render_template, request, jsonify
+import os
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
+
+if os.path.exists("env.py"):
+    import env
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
+        debug=True)
