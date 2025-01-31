5e8fbe95a747fa01b2b7db294549b085be35ce85
Ionut Blanaru <blanaru.ionut@gmail.com>
2025-01-31 09:28:51 +0000

    Added the sign-up page and login page

--- app.py
+++ app.py
@@ -18,6 +18,11 @@ def sign_up():
     return render_template('sign-up.html')
 
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
         port=int(os.environ.get("PORT")),
